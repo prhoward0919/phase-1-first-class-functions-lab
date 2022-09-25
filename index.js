e4a2d3b22aa84e11a3ea84bf4d44a9c2cf848949
Paige <prhoward97@gmail.com>
2022-09-25 19:45:47 -0400

    commit tree 406ef7d834f1b53176ec7af7d1fe8b635b6d3d1e

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Code your solution in this file!
+const arrayOfDrivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function(arrayOfDrivers) {
+   
+    const firstDrivers = arrayOfDrivers.slice(0,2);
+    return firstDrivers;
+}
+
+
+const returnLastTwoDrivers = function(arrayOfDrivers) {
+   
+    const lastDrivers = arrayOfDrivers.slice(2,4);
+    return lastDrivers;
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(fare) {
+    return function(multiplier) {
+        return multiplier * fare;
+    };
+}
+
+function fareDoubler(fare) {
+    return createFareMultiplier(fare)(2);
+}
+function fareTripler(fare) {
+    return createFareMultiplier(fare)(3);
+}
+
+function selectDifferentDrivers(arrayOfDrivers, functionName) {
+    let theDrivers;
+    if (functionName === returnFirstTwoDrivers) {
+        theDrivers = returnFirstTwoDrivers(arrayOfDrivers);
+    } else if (functionName === returnLastTwoDrivers) {
+        theDrivers = returnLastTwoDrivers(arrayOfDrivers);
+    }
+    return theDrivers;
+        
+    }
